7a5a525eea7b41e1a75b47a9ba8e353bcf8a1aad
somesh <s7911999@gmail.com>
2024-11-14 13:44:45 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Layout from "./components/Layout"
+import videocap from "./components/videocap"
 function App() {
   return (
     <>
    <Layout/>
+   <videocap/>
    
     </>
   )
 }
 
 export default App
+
+
