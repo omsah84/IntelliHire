a09e28262127e9aaba59aa3340908732ae14fde4
OMSAH <huncyoms@gmail.com>
2024-11-15 01:42:24 -0500

    commited

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import "./App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './component/Home';
-import StartInterView from "./component/StartInterview";
 
 export default function App() {
   return (
