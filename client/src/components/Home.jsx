5e4a322272d555745b79ef6f97ca1fd6c74a863f
kavya <kavyasreeyadav20@gmail.com>
2024-11-16 15:11:25 +0530

    added

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.css"
+export default function Home() {
+  return (
+    <div className="home">
+      <h2 style={{fontSize:"50px"}}>welcome to virtual Interview Assistant</h2>
+      <button className="btn">start</button>
+    </div>
+
+  )
+}
