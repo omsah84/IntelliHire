0e24f6573c0ae8e482175baf961122b11ccb9bbb
Durga15504 <durgabhavanikolla7@gmail.com>
2024-11-16 14:33:11 +0530

    added

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./home.css"
+export default function Home() {
+  return (
+    <div className='home'>
+      <h4>Welcome to Virtual Interview Assistant</h4>
+      <button className='btn'>Start</button>
+    </div>
+  )
+}
+  
+ 
+  
