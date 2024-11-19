a67ebaed38e192ec1e17eb5cdd0320977cdd6de0
kavya <kavyasreeyadav20@gmail.com>
2024-11-19 20:01:19 +0530

    added

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./layout.css"
+export default function Home() {
+    return (
+        <>
+
+             <div className='flex'></div>
+                <div className='left'>Left</div>
+                <div className='right'>Right</div>
+            </div>
+            
+            <div>
+                <button className='btn'>End</button> 
+            </div>
+        </>
+   
+    )
+}
