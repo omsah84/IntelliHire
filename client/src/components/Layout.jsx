0c8e2c57676b8ee6111b553860251087c54104de
Durga15504 <durgabhavanikolla7@gmail.com>
2024-11-20 16:30:14 +0530

    added

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import Webcam from 'react-webcam';
+import { useRef } from 'react';
+import logo from "../assets/logo.png";
 import "./layout.css"
-export default function Home() {
+export default function layout() {
+
+    const webcamRef = useRef(null);
+
     return (
         <>
 
             <div className='flex'>
-                <div className='left'>Left</div>
-                <div className='right'>Right</div>
+                <div className='left'>
+
+                    <Webcam
+                        audio={false}
+                        ref={webcamRef}
+                        screenshotFormat="image/jpeg"
+                        className='webcam'
+                    />
+                </div>
+                <div className='right'>
+                    <img className='pic' src={logo}/>
+                    <img className='pic' src={logo}/>
+                    <img className='pic' src={logo}/>
+                    <img className='pic' src={logo}/>
+                    <img className='pic' src={logo}/>
+                    <img className='pic' src={logo}/>
+
+                </div>
             </div>
 
-            <div>
+            <div style={{ textAlign: "center" }}>
                 <button className='btn'>End</button>
             </div>
         </>
