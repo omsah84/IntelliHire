1d06a74bb8f6a01ca1d0485f703650553e664a9d
Durga15504 <durgabhavanikolla7@gmail.com>
2024-11-21 16:14:55 +0530

    added

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,8 +1,27 @@
 import Webcam from 'react-webcam';
+import React, { useState, useEffect } from 'react';
 import { useRef } from 'react';
 import logo from "../assets/logo.png";
 import "./layout.css"
 export default function layout() {
+    const [timeElapsed, setTimeElapsed] = useState(0);
+
+    useEffect(() => {
+
+        const timerId = setInterval(() => {
+            setTimeElapsed(prevTime => prevTime + 1);
+        }, 1000);
+
+        return () => {
+            clearInterval(timerId);
+        };
+    }, []);
+
+    const formatTime = (seconds) => {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${minutes} minutes ${remainingSeconds} seconds`;
+    };
 
     const webcamRef = useRef(null);
 
@@ -18,14 +37,18 @@ export default function layout() {
                         screenshotFormat="image/jpeg"
                         className='webcam'
                     />
+
+                    <div className='result'>
+                        <div>Time Elapsed:  {formatTime(timeElapsed)} seconds - 2 mins</div>
+                    </div>
                 </div>
                 <div className='right'>
-                    <img className='pic' src={logo}/>
-                    <img className='pic' src={logo}/>
-                    <img className='pic' src={logo}/>
-                    <img className='pic' src={logo}/>
-                    <img className='pic' src={logo}/>
-                    <img className='pic' src={logo}/>
+                    <img className='pic' src={logo} />
+                    <img className='pic' src={logo} />
+                    <img className='pic' src={logo} />
+                    <img className='pic' src={logo} />
+                    <img className='pic' src={logo} />
+                    <img className='pic' src={logo} />
 
                 </div>
             </div>
