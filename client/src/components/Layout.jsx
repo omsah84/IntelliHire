e1253ce5fd29e46b8662570fa27aa2da2ef032ce
Jagadesh Mylagani <jagadeesh.mg421@gmail.com>
2024-11-21 22:08:03 +0530

    added

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,10 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import logo from '../assets/logo.png'
 import Webcam from 'react-webcam';
 import { useRef } from 'react';
 import "./layout.css"
 export default function Layout() {
+    const [timeElapsed, setTimeElapsed] = useState(0);
+
+    useEffect(()=>{
+        
+        const timerId = setInterval(() => {
+            setTimeElapsed(e => e + 1);
+        }, 1000);
+
+       return () => {
+        clearInterval(timerId);
+    };
+    },[]);
 
     const webcamRef = useRef(null);
 
+    const formatTime = (seconds) => {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${minutes} minutes ${remainingSeconds} seconds`;
+    };
+    
     return (
         <div>
             <div className="container">
@@ -15,8 +35,19 @@ export default function Layout() {
                             screenshotFormat="image/jpeg"
                             className='webcam'
                         />
+                        <div className='result'>
+                        <div>Time Elapsed:  {formatTime(timeElapsed)} seconds - 2 mins</div>
+                        
                 </div>
-                <div className="right">right</div>
+
+                </div>
+                <div className="right">
+                <img className="pic" src= {logo} alt="" />
+                <img className="pic" src= {logo} alt="" />
+                <img className="pic" src= {logo} alt="" />
+                <img className="pic" src= {logo} alt="" />
+                <img className="pic" src= {logo} alt="" />
+                </div>
             </div>
             <div style={{textAlign:"center"}}>
                 <button className="btn">End</button>
